daf6fbbbbc7cf3d0fb0920c02f36a0cd931c59bb
keshancp <keshanc.perera@gmail.com>
2022-10-04 21:04:17 +0530

    initial commit for ecomm-auth

---
+++ src/main/java/com/ecomm/ecommauth/EcommAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommAuthApplication.class, args);
+	}
+
+}
